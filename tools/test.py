e56bede2a3d685e6e1d0d3efec20e6b5469877f4
ssbagalkar <saurabh@alegion.com>
2020-12-15 18:13:50 +0000

    modify paths and remove OPE

--- tools/test.py
+++ tools/test.py
@@ -34,6 +34,15 @@ parser.add_argument('--threshold', default=0.85, type=float,
                     help='IOU minimum threshold')
 parser.add_argument('--model_name', default='', type=str,
                     help='Model architecture')
+parser.add_argument('--skip_frames', default=1, type=int,
+                    help='number of frames to skip after failure')
+parser.add_argument('--experiment_name', default='', type=str,
+                    help='what experiment is this')
+parser.add_argument('--results_path', default='', type=str,
+                    help='absolute path of results directory')
+parser.add_argument('--dataset_directory', default='', type=str,
+                    help='absolute path of dataset directory')
+
 args = parser.parse_args()
 
 torch.set_num_threads(1)
@@ -50,11 +59,8 @@ def main():
 
     # load config
     cfg.merge_from_file(args.config)
-
-    # cur_dir = os.path.dirname(os.path.realpath(__file__))
-    # dataset_root = os.path.join(cur_dir, '../testing_dataset', args.dataset)
-    # dataset_root = os.path.join('/media/saurabh/Elements/raw_data', args.dataset)
-    dataset_root = os.path.join('/media/saurabh/Elements/raw_data/umich/Sauron-data/Sauron-November-data', args.dataset)
+    dataset_root = os.path.join(args.dataset_directory, args.dataset)
+    print(f"dataset root-->{dataset_root}")
 
     # create model
     model = ModelBuilder()
@@ -71,6 +77,8 @@ def main():
                                             load_img=False)
 
     model_name = args.model_name
+    print(f"Model name is {model_name}")
+
     total_lost = 0
     if args.dataset in vot_like_dataset:
         # restart tracking
@@ -108,7 +116,7 @@ def main():
                     else:
                         # lost object
                         pred_bboxes.append(2)
-                        frame_counter = idx + 1 # skip 1 frame
+                        frame_counter = idx + args.skip_frames # skip 1 frame
                         lost_number += 1
                 else:
                     pred_bboxes.append(0)
@@ -131,18 +139,17 @@ def main():
                     cv2.waitKey(1)
             toc /= cv2.getTickFrequency()
             # save results
-            video_path = os.path.join(os.path.join(dataset_root, '../', 'November-results/ryan-jeremy-demo-dummy'),
-                                      args.dataset, model_name, video.name)
-            if not os.path.isdir(video_path):
-                os.makedirs(video_path)
-            result_path = os.path.join(video_path, '{}_001.txt'.format(video.name))
+            save_path = os.path.join(args.results_path, args.dataset, model_name, video.name)
+            if not os.path.isdir(save_path):
+                os.makedirs(save_path)
+            result_path = os.path.join(save_path, '{}_001.txt'.format(video.name))
             with open(result_path, 'w') as f:
                 for x in pred_bboxes:
                     if isinstance(x, int):
                         f.write("{:d}\n".format(x))
                     else:
                         f.write(','.join([vot_float2str("%.4f", i) for i in x])+'\n')
-            with open(os.path.join(video_path, '..', 'lost.txt'), 'a+') as f:
+            with open(os.path.join(save_path, '..', 'lost.txt'), 'a+') as f:
                 f.write(f"{v_idx+1} Class: {video.name} | Time: {toc}s | Speed: {idx/toc}fps | Lost:{lost_number}  \n")
 
             print('({:3d}) Class: {:12s} Time: {:4.1f}s Speed: {:3.1f}fps Lost: {:d}'.format(
@@ -151,91 +158,7 @@ def main():
         print("{:s} total lost: {:d}".format(model_name, total_lost))
     else:
         # OPE tracking
-        for v_idx, video in enumerate(dataset):
-            if args.video != '':
-                # test one special video
-                if video.name != args.video:
-                    continue
-            toc = 0
-            pred_bboxes = []
-            scores = []
-            track_times = []
-            for idx, (img, gt_bbox) in enumerate(video):
-                tic = cv2.getTickCount()
-                if idx == 0:
-                    cx, cy, w, h = get_axis_aligned_bbox(np.array(gt_bbox))
-                    gt_bbox_ = [cx-(w-1)/2, cy-(h-1)/2, w, h]
-                    tracker.init(img, gt_bbox_)
-                    pred_bbox = gt_bbox_
-                    scores.append(None)
-                    if 'VOT2018-LT' == args.dataset:
-                        pred_bboxes.append([1])
-                    else:
-                        pred_bboxes.append(pred_bbox)
-                else:
-                    outputs = tracker.track(img)
-                    pred_bbox = outputs['bbox']
-                    pred_bboxes.append(pred_bbox)
-                    scores.append(outputs['best_score'])
-                toc += cv2.getTickCount() - tic
-                track_times.append((cv2.getTickCount() - tic)/cv2.getTickFrequency())
-                if idx == 0:
-                    cv2.destroyAllWindows()
-                if args.vis and idx > 0:
-                    gt_bbox = list(map(int, gt_bbox))
-                    pred_bbox = list(map(int, pred_bbox))
-                    cv2.rectangle(img, (gt_bbox[0], gt_bbox[1]),
-                                  (gt_bbox[0]+gt_bbox[2], gt_bbox[1]+gt_bbox[3]), (0, 255, 0), 3)
-                    cv2.rectangle(img, (pred_bbox[0], pred_bbox[1]),
-                                  (pred_bbox[0]+pred_bbox[2], pred_bbox[1]+pred_bbox[3]), (0, 255, 255), 3)
-                    cv2.putText(img, str(idx), (40, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
-                    cv2.imshow(video.name, img)
-                    cv2.waitKey(1)
-            toc /= cv2.getTickFrequency()
-            # save results
-            if 'VOT2018-LT' == args.dataset:
-                video_path = os.path.join('../experiments/siamrpn_r50_l234_dwxcorr_8gpu/results', args.dataset, model_name,
-                        'longterm', video.name)
-                if not os.path.isdir(video_path):
-                    os.makedirs(video_path)
-                result_path = os.path.join(video_path,
-                        '{}_001.txt'.format(video.name))
-                with open(result_path, 'w') as f:
-                    for x in pred_bboxes:
-                        f.write(','.join([str(i) for i in x])+'\n')
-                result_path = os.path.join(video_path,
-                        '{}_001_confidence.value'.format(video.name))
-                with open(result_path, 'w') as f:
-                    for x in scores:
-                        f.write('\n') if x is None else f.write("{:.6f}\n".format(x))
-                result_path = os.path.join(video_path,
-                        '{}_time.txt'.format(video.name))
-                with open(result_path, 'w') as f:
-                    for x in track_times:
-                        f.write("{:.6f}\n".format(x))
-            elif 'GOT-10k' == args.dataset:
-                video_path = os.path.join('../experiments/siamrpn_r50_l234_dwxcorr_8gpu/results', args.dataset, model_name, video.name)
-                if not os.path.isdir(video_path):
-                    os.makedirs(video_path)
-                result_path = os.path.join(video_path, '{}_001.txt'.format(video.name))
-                with open(result_path, 'w') as f:
-                    for x in pred_bboxes:
-                        f.write(','.join([str(i) for i in x])+'\n')
-                result_path = os.path.join(video_path,
-                        '{}_time.txt'.format(video.name))
-                with open(result_path, 'w') as f:
-                    for x in track_times:
-                        f.write("{:.6f}\n".format(x))
-            else:
-                model_path = os.path.join('../experiments/siamrpn_r50_l234_dwxcorr_8gpu/results', args.dataset, model_name)
-                if not os.path.isdir(model_path):
-                    os.makedirs(model_path)
-                result_path = os.path.join(model_path, '{}.txt'.format(video.name))
-                with open(result_path, 'w') as f:
-                    for x in pred_bboxes:
-                        f.write(','.join([str(i) for i in x])+'\n')
-            print('({:3d}) Video: {:12s} Time: {:5.1f}s Speed: {:3.1f}fps'.format(
-                v_idx+1, video.name, toc, idx / toc))
+        # will be implemented if needed in future
 
 
 if __name__ == '__main__':
